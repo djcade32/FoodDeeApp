c41d3a636676b749362674765a3f6c542edd3f77
Norman <norman_cade32@yahoo.com>
2022-05-12 16:33:08 -0400

    init files

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
+// Loading fonts
+function fetchFonts() {
+  return Font.loadAsync({
+    lato: require("./assets/fonts/Lato-Regular.ttf"),
+    "lato-bold": require("./assets/fonts/Lato-Bold.ttf"),
+    playfairDisplay: require("./assets/fonts/PlayfairDisplay-Regular.ttf"),
+    "playfairDisplay-medium": require("./assets/fonts/PlayfairDisplay-Medium.ttf"),
+    "playfairDisplay-bold": require("./assets/fonts/PlayfairDisplay-Bold.ttf"),
+  });
+}
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      // Make sure all fonts are loaded before app shows screen
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +39,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
