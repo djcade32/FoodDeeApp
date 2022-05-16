96a18e60d132d60583797e8088cf1367635b38d0
Norman Cade <norman_cade32@yahoo.com>
2022-05-16 16:00:17 -0400

    implementing bottom tab on restaurant screen

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -71,4 +71,19 @@ const HomeTabs = () => {
       />
     </Tab.Navigator>
   );
+
+  const HomeStackNavigator = () => {
+    return (
+      <HomeStack.Navigator>
+        <HomeStack.Screen name="Restaurants" component={HomeScreen} />
+        <HomeStack.Screen
+          name="Restaurant"
+          component={RestaurantDetailsScreen}
+          options={{ headerShown: false }}
+        />
+        <HomeStack.Screen name="Dish" component={DishDetailsScreen} />
+        <HomeStack.Screen name="Basket" component={Basket} />
+      </HomeStack.Navigator>
+    );
+  };
 };
