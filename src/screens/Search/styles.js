9ec90d12c70f06d15a41a9806315a3673a9ee615
Norman <norman_cade32@yahoo.com>
2022-06-21 13:28:40 -0400

    Added loading text

--- src/screens/Search/styles.js
+++ src/screens/Search/styles.js
@@ -1,3 +1,10 @@
 import { StyleSheet } from "react-native";
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  loadingText: {
+    color: "gray",
+    fontFamily: "lato",
+    fontSize: 16,
+    textAlign: "center",
+  },
+});
